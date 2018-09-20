e3723127ea4d7fe88e7a3fcac6d474cba758db8d
Matthew Werdean <matthew.werdean@basised.com>
2018-09-19 18:25:10 -0700

    deleted old hero

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import Header from './components/header'
 import Box from './components/box'
-import Hero2 from './components/hero2'
+import Hero from './components/hero'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 
-      <Hero2 />
+      <Hero />
       </div>
 
     );
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,55 +1,51 @@
 import React, { Component } from 'react'
-import bird from '../assets/bird.png'
-import layer1 from '../assets/Layer1.png'
-import layer2 from '../assets/Layer2.png'
-import layer3 from '../assets/Layer3.png'
-import layer4 from '../assets/Layer4.png'
-import sky from '../assets/sky2.png'
-import name from '../assets/name.png'
 
 export default class Hero extends Component {
     constructor(props) {
-        super(props)
-        this.myRef = React.createRef()
+        super(props),
+        this.state = {
+            transform: (0, 0),
+        }
     }
-
     componentDidMount() {
         window.addEventListener('scroll', this.handleScroll)
     }
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
 
     handleScroll = (event) => {
-        var wScroll = event.srcElement.body.scrollTop
-        console.log(wScroll)
+        let scrollTop = event.path[1].scrollY,
+        itemTranslate = ( scrollTop/2 + 'px');
+
+    this.setState({
+      transform: itemTranslate
+    });
     }
 
   render() {
     return (
-        <div>
-            <div className="hero-container">
-                <div className= "parallax hero-name">
-                    <img src = {name} alt = "name" />
-                </div>
-                <div className= "parallax hero-sky">
-                    <img src = {sky} alt = "sky" />
-                </div>
-                <div className="parallax hero-bird">
-                    <img src = {bird} alt = "bird" />
-                </div>
-                <div className="parallax hero-layer1">
-                    <img src = {layer1} alt = "layer1" />
-                </div>
-                <div className="parallax hero-layer2">
-                    <img src = {layer2} alt = "layer2" />
-                </div>
-                <div className="parallax hero-layer3">
-                    <img src = {layer3} alt = "layer3" />
-                </div>
-                <div className="parallax hero-layer4">
-                    <img src = {layer4} alt = "layer4" />
-                </div>
-            </div>
-            <div className="testbox"></div>
-            <div className="testbox"></div>
+      <div>
+
+        <header className = "hero-box">
+            <div className = "name" style = {{transform: `translate(0, ${this.state.transform})`}}></div>
+            <div className = "fore-bird" style = {{transform: `translate(${this.state.transform}, -${this.state.transform})`}}></div>
+            <div className = "layer1"></div>
+            <div className = "layer2"></div>
+            <div className = "layer3"></div>
+            <div className = "layer4"></div>
+        </header>
+
+        <section className = "content">
+            <article>
+                <h1>Matthew Werdean</h1>
+                <hr />
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </article>
+        </section>
+        <div style={{height: 2000}}></div>
       </div>
     )
   }
