8fde2a4f7a8b2d690a5031852cd0a25d1d8b719f
unknown <mwerdean@gmail.com>
2018-09-20 20:29:24 -0700

    added birds

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
 export default class Hero extends Component {
     constructor(props) {
         super(props),
         this.state = {
             transform: (0, 0),
+            transform2: (0, 0),
+            menuShow: false
         }
+        this.myRef = React.createRef()
     }
     componentDidMount() {
         window.addEventListener('scroll', this.handleScroll)
@@ -17,9 +21,13 @@ export default class Hero extends Component {
     handleScroll = (event) => {
         let scrollTop = event.path[1].scrollY,
         itemTranslate = ( scrollTop/2 + 'px');
-
+        if(event.path[1].scrollY >= 1000) {
+            this.setState({ menuShow: true })
+        }
+        console.log(scrollTop)
     this.setState({
-      transform: itemTranslate
+      transform: itemTranslate,
+      transform2: (scrollTop/10 + 'px')
     });
     }
 
@@ -30,13 +38,14 @@ export default class Hero extends Component {
         <header className = "hero-box">
             <div className = "name" style = {{transform: `translate(0, ${this.state.transform})`}}></div>
             <div className = "fore-bird" style = {{transform: `translate(${this.state.transform}, -${this.state.transform})`}}></div>
+            <div className = "back-bird" style = {{transform: `translate(${this.state.transform2}, -${this.state.transform2})`}}></div>
             <div className = "layer1"></div>
             <div className = "layer2"></div>
             <div className = "layer3"></div>
             <div className = "layer4"></div>
         </header>
 
-        <section className = "content">
+        <section ref = {this.myRef} className = "content">
             <article>
                 <h1>Matthew Werdean</h1>
                 <hr />
